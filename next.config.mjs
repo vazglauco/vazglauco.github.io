ffe837013cfbf3b1920c6950e3ce4302c7c325aa
Glauco Vaz <vazz.glauco@gmail.com>
2025-07-27 15:59:08 -0300

    fix dist dir

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'dist',
+  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
